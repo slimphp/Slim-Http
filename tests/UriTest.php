28d09ab11259a25a71ecc6b6bfe656de9f087031
Daniel Opitz <git@copynpaste.de>
2017-11-26 01:05:29 +0100

    forward port slimphp/Slim#2332

--- tests/UriTest.php
+++ tests/UriTest.php
@@ -159,6 +159,15 @@ class UriTest extends TestCase
         $this->assertAttributeEquals('', 'password', $uri);
     }
 
+    public function testWithUserInfoRemovesInfo()
+    {
+        $uri = $this->uriFactory()->withUserInfo('bob', 'password');
+
+        $uri = $uri->withUserInfo('');
+        $this->assertAttributeEquals('', 'user', $uri);
+        $this->assertAttributeEquals('', 'password', $uri);
+    }
+
     public function testGetHost()
     {
         $this->assertEquals('example.com', $this->uriFactory()->getHost());
